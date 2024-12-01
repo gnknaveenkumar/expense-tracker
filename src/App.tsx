99b2a1ed89e8b0d946d134ea117cd4165afb941a
Naveen Kumar G <gnknaveen1994@gmail.com>
2024-12-02 00:36:59 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center p-8 bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS with React and TypeScript!</h1>
+
+    </div>
+  );
+}
+
+export default App;
